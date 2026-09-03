f65c9fb5666286f32ab6ce264e2c243e7177d212
israelheredia1986 <israelheredia1986@gmail.com>
2026-09-03 16:30:27 +0200

    Actualizar caché PWA y añadir buscadores globales/infracciones

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "centinela-code-v6";
+const CACHE_NAME = "centinela-code-v7";
 
 const ARCHIVOS = [
  "./",
  "./index.html",
  "./style.css",
  "./app.js",
+ "./buscadores.js",
  "./manifest.json",
-
  "./data/lopsc.json",
  "./data/infracciones.json",
  "./data/infracciones_trafico.json",
@@ -23,52 +23,45 @@ const ARCHIVOS = [
  "./data/espectaculos_publicos.json",
  "./data/medio_ambiente_ruidos.json",
  "./data/reglamento_armas.json",
- "./data/policias_locales_andalucia.json"
+ "./data/policias_locales_andalucia.json",
+ "./data/ley_39_2015.json",
+ "./data/ley_7_1985.json",
+ "./data/ley_5_2010_andalucia.json"
 ];
 
-
 self.addEventListener("install", event => {
-
- console.log("Centinela SW instalado");
-
- self.skipWaiting();
-
- event.waitUntil(
-  caches.open(CACHE_NAME)
-  .then(cache => cache.addAll(ARCHIVOS))
- );
-
+  console.log("Centinela SW instalado v7");
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ARCHIVOS))
+  );
 });
-
 
 self.addEventListener("activate", event => {
-
- console.log("Centinela SW activo");
-
- event.waitUntil(
-
-  caches.keys()
-  .then(keys =>
-   Promise.all(
-    keys
-    .filter(k => k !== CACHE_NAME)
-    .map(k => caches.delete(k))
-   )
-  )
-  .then(() => self.clients.claim())
-
- );
-
+  console.log("Centinela SW activo v7");
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
 });
 
-
 self.addEventListener("fetch", event => {
+  const request = event.request;
+  if (request.method !== "GET") return;
 
- event.respondWith(
-
-  fetch(event.request)
-  .catch(() => caches.match(event.request))
-
- );
-
+  event.respondWith(
+    fetch(request)
+      .then(response => {
+        const url = new URL(request.url);
+        if (url.origin === self.location.origin) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(request, copy)).catch(() => {});
+        }
+        return response;
+      })
+      .catch(() => caches.match(request))
+  );
 });
